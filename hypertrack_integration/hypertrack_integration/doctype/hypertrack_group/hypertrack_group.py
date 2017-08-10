9326825bbcf239c1012c1d48987405a16434adb3
Revant Nandgaonkar <revant.one@gmail.com>
2017-08-10 22:04:53 +0530

    Group Tree added

--- hypertrack_integration/hypertrack_integration/doctype/hypertrack_group/hypertrack_group.py
+++ hypertrack_integration/hypertrack_integration/doctype/hypertrack_group/hypertrack_group.py
@@ -9,7 +9,7 @@ from frappe.utils.nestedset import NestedSet
 from hypertrack_integration.hypertrack_integration.doctype.hypertrack_settings.hypertrack_settings import get_hypertrack 
 
 class HyperTrackGroup(NestedSet):
-	nsm_parent_field = "parent_hypertrack_group";
+	nsm_parent_field = "parent_hypertrack_group"
 	
 	def after_insert(self):
 		hypertrack = get_hypertrack()
@@ -28,36 +28,3 @@ class HyperTrackGroup(NestedSet):
 	def on_delete(self):
 		hypertrack = get_hypertrack()
 		group = hypertrack.Group.delete()
-		
-
-@frappe.whitelist()
-def get_children():
-	ctype = frappe.local.form_dict.get('ctype')
-	parent_field = 'parent_' + ctype.lower().replace(' ', '_')
-	parent = frappe.form_dict.get("parent") or ""
-
-	return frappe.db.sql("""select name as value,
-		if(is_group='Yes', 1, 0) as expandable
-		from `tab{ctype}`
-		where docstatus < 2
-		and ifnull(`{parent_field}`,'') = %s
-		order by name""".format(ctype=frappe.db.escape(ctype), parent_field=frappe.db.escape(parent_field)),
-		parent, as_dict=1)
-
-
-@frappe.whitelist()
-def add_node():
-	ctype = frappe.form_dict.get('ctype')
-	parent_field = 'parent_' + ctype.lower().replace(' ', '_')
-	name_field = ctype.lower().replace(' ', '_') + '_name'
-
-	doc = frappe.new_doc(ctype)
-	doc.update({
-		name_field: frappe.form_dict['name_field'],
-		parent_field: frappe.form_dict['parent'],
-		"is_group": frappe.form_dict['is_group']
-	})
-
-	doc.save()
-	frappe.db.commit()
-
--- hypertrack_integration/hypertrack_integration/doctype/hypertrack_group/hypertrack_group_tree.js
+++ hypertrack_integration/hypertrack_integration/doctype/hypertrack_group/hypertrack_group_tree.js
@@ -1,13 +1,2 @@
 frappe.treeview_settings["HyperTrack Group"] = {
-	breadcrumbs: "HyperTrack Integration",
-	get_tree_root: false,
-	root_label: "All HyperTrack Groups",
-	get_tree_nodes: 'hypertrack_integration.doctype.hypertrack_group.hypertrack_group.get_children',
-	add_tree_node: 'hypertrack_integration.doctype.hypertrack_group.hypertrack_group.add_node',
-	fields:[
-		{fieldtype:'Data', fieldname:'hypertrack_group_name', label:__('New HyperTrack Group Name'), reqd:true},
-		{fieldtype:'Check', fieldname:'is_group', label:__('Is Group'),
-			description:__('Further HyperTrack can be made under Groups but entries can be made against non-Groups')}
-	],
-	ignore_fields:["parent_hypertrack_group"]
 }
