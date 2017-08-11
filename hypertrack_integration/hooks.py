1f404489c9d5e151f2a449f6c942cc586296757f
Revant Nandgaonkar <revant.one@gmail.com>
2017-08-11 13:41:37 +0530

    added web tracking js

--- hypertrack_integration/hooks.py
+++ hypertrack_integration/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/hypertrack_integration/css/hypertrack_integration.css"
-# app_include_js = "/assets/hypertrack_integration/js/hypertrack_integration.js"
+app_include_js = ["/assets/hypertrack_integration/js/track.js"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/hypertrack_integration/css/hypertrack_integration.css"
