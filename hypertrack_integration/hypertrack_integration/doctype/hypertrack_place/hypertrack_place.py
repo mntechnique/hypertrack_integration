d4f1b33ba325b33d57023d4ef5218784d0ed95a8
Gaurav Naik <gvrvnk@gmail.com>
2017-08-12 22:06:18 +0530

    Hypertrack User saving enabled. Methods on other entities

--- hypertrack_integration/hypertrack_integration/doctype/hypertrack_place/hypertrack_place.py
+++ hypertrack_integration/hypertrack_integration/doctype/hypertrack_place/hypertrack_place.py
@@ -5,6 +5,38 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from hypertrack_integration.hypertrack_integration.doctype.hypertrack_settings.hypertrack_settings import get_hypertrack 
 
 class HyperTrackPlace(Document):
-	pass
+	def before_insert(self):
+		hypertrack = get_hypertrack()
+		new_hypertrack_place = hypertrack.Action.create( \
+			name=self.hypertrack_name, \
+			parent_group_id = self.parent_hypertrack_group)
+
+		self.hypertrack_name = new_hypertrack_place.name
+		self.address = new_hypertrack_place.address
+		self.landmark = new_hypertrack_place.landmark
+		self.zip_code = new_hypertrack_place.zip_code
+		self.city = new_hypertrack_place.city
+		self.state = new_hypertrack_place.state
+		self.country = new_hypertrack_place.country
+		self.location = new_hypertrack_place.location
+
+	def on_update(self):
+		hypertrack = get_hypertrack()
+		action = hypertrack.Action.retrieve(self.hypertrack_id)
+
+		action.type = self.type
+		action.lookup_id = self.lookup_id
+		action.scheduled_at = self.scheduled_at
+		action.expected_place = self.expected_place
+		action.metadata = self.metadata
+
+		action.save()
+
+	def on_delete(self):
+		hypertrack = get_hypertrack()
+		action = hypertrack.Action.retrieve(self.hypertrack_id)
+		action.delete():
+
--- hypertrack_integration/hypertrack_integration/doctype/hypertrack_user/hypertrack_user.js
+++ hypertrack_integration/hypertrack_integration/doctype/hypertrack_user/hypertrack_user.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('HyperTrack User', {
+	onload: function(frm) {
+		frm.add_fetch("frappe_user", "full_name", "hypertrack_name");
+		frm.add_fetch("frappe_user", "mobile_no", "phone");
+		frm.add_fetch("frappe_user", "name", "lookup_id");
+		frm.set_query("group_id", function() {
+			return {
+				filters: [["name", "!=", "All HyperTrack Groups"]]
+			}
+		})
+	},
 	refresh: function(frm) {
 
-	}
+	},
 });
