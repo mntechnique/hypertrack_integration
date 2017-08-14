dd1e3dc0c07d85297850459ab769c65231c8f910
Revant Nandgaonkar <revant.one@gmail.com>
2017-08-14 23:14:27 +0530

    Group add/delete

--- hypertrack_integration/hypertrack_integration/doctype/hypertrack_group/hypertrack_group.py
+++ hypertrack_integration/hypertrack_integration/doctype/hypertrack_group/hypertrack_group.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 import requests
+import json
 from frappe.model.document import Document
 from frappe.utils.nestedset import NestedSet
 from hypertrack_integration.hypertrack_integration.doctype.hypertrack_settings.hypertrack_settings import get_hypertrack 
@@ -13,44 +14,61 @@ class HyperTrackGroup(NestedSet):
 	nsm_parent_field = "parent_hypertrack_group"
 	
 	def validate(self):
-		if self.hypertrack_group_name != "All HyperTrack Groups":
+		if self.hypertrack_group_name != "All HyperTrack Groups" and not self.hypertrack_id:
 			hypertrack = HTGroup(frappe.db.get_value("HyperTrack Settings",None, "hypertrack_secret_key"))
+			parent_group_id = None
+			if self.parent_hypertrack_group and self.parent_hypertrack_group != "All HyperTrack Groups":
+				parent_group_id = frappe.db.get_value("HyperTrack Group", self.parent_hypertrack_group, "hypertrack_id")
+
 			new_hypertrack_group = hypertrack.create(
 				name = self.hypertrack_group_name,
-				parent_group_id = self.parent_hypertrack_group
+				parent_group_id = parent_group_id
 			)
+
+			self.created_at = new_hypertrack_group.get("created_at")
+			self.hypertrack_id = new_hypertrack_group.get("id")
+			self.modified_at = new_hypertrack_group.get("modified_at")
+			self.hypertrack_group_name = new_hypertrack_group.get("name")
+
+			parent_hypertrack_group = self.parent_hypertrack_group \
+				if self.parent_hypertrack_group == "All HyperTrack Groups" else None
+
+			if new_hypertrack_group.get("parent_group_id"):
+				parent_hypertrack_group = frappe.db.get_value("HyperTrack Group", {"hypertrack_id": new_hypertrack_group.get("parent_group_id")}, "hypertrack_group_name")
+
+			self.parent_hypertrack_group = parent_hypertrack_group
+			self.token = new_hypertrack_group.get("token")
+
+		elif self.hypertrack_group_name != "All HyperTrack Groups" and self.hypertrack_id:
+			hypertrack = HTGroup(frappe.db.get_value("HyperTrack Settings", None, "hypertrack_secret_key"))
+			updated_group = hypertrack.update(self.hypertrack_id, self.hypertrack_group_name)
 			for x in xrange(1,10):
-				print(new_hypertrack_group)
+				print(updated_group or "Nogroup")
+			self.hypertrack_group_name = updated_group.get("name")
+			self.modified_at = updated_group.get("modified_at")
 
-	# def on_update(self):
-	# 	if self.hypertrack_group_name != "All HyperTrack Groups":
-	# 		hypertrack = HTGroup(frappe.db.get_value("HyperTrack Settings", None, "hypertrack_secret_key"))
-	# 		group = hypertrack.retrieve(self.hypertrack_id)
-
-	# 		if self.hypertrack_group_name:
-	# 			group.name = self.hypertrack_group_name
-	# 		group.save()
-
-	def on_delete(self):
+	def on_trash(self):
 		if self.hypertrack_group_name != "All HyperTrack Groups":
-			hypertrack = get_hypertrack()
-			group = hypertrack.Group.delete()
+			hypertrack = HTGroup(frappe.db.get_value("HyperTrack Settings",None, "hypertrack_secret_key"))
+			group = hypertrack.delete(self.hypertrack_id)
 
 class HTGroup():
 	def __init__(self, sk):
 		self.headers = {
-			'Authorization': sk,
+			'Authorization': "token " + sk,
 			'Content-Type': 'application/json',
 		}
 		self.group_base_url = 'https://api.hypertrack.com/api/v1/groups/'
 
-	def create(self, name, parent_group_id):
+	def create(self, name, parent_group_id=None):
 		data = {
-			"name": name,
-			"parent_group_id": parent_group_id
+			"name": name
 		}
+		if parent_group_id:
+			data["parent_group_id"] = parent_group_id
+
 		try:
-			response = requests.post(self.group_base_url, headers=self.headers, data=data)
+			response = requests.post(self.group_base_url, headers=self.headers, data=json.dumps(data))
 			return response.json()
 		except Exception as e:
 			raise e
@@ -65,6 +83,23 @@ class HTGroup():
 	def delete(self, group_uuid):
 		try:
 			response = requests.delete(self.group_base_url + group_uuid, headers=self.headers)
+			return response.text
+		except Exception as e:
+			raise e
+
+	def list(self):
+		try:
+			response = requests.get(self.group_base_url, headers=self.headers)
+			return response.json()
+		except Exception as e:
+			raise e
+
+	def update(self, group_uuid, changed_group_name):
+		data = {
+			"name": changed_group_name
+		}
+		try:
+			response = requests.patch(self.group_base_url + group_uuid, headers=self.headers, data=json.dumps(data))
 			return response.json()
 		except Exception as e:
 			raise e
