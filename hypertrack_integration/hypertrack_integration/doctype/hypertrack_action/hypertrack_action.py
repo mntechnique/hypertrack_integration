fff891be6db2161c037b261a0c9d8bb073690847
Revant Nandgaonkar <revant.one@gmail.com>
2017-08-14 19:03:35 +0530

    Add/delete Action

--- hypertrack_integration/hypertrack_integration/doctype/hypertrack_action/hypertrack_action.py
+++ hypertrack_integration/hypertrack_integration/doctype/hypertrack_action/hypertrack_action.py
@@ -3,7 +3,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, pytz
 from frappe.model.document import Document
 from hypertrack_integration.hypertrack_integration.doctype.hypertrack_settings.hypertrack_settings import get_hypertrack 
 
@@ -11,27 +11,34 @@ class HyperTrackAction(Document):
 	
 	def validate(self):
 		hypertrack = get_hypertrack()
-		new_hypertrack_action = hypertrack.Action.create(\
-			type=self.type,
-			scheduled_at=self.scheduled_at,
-			expected_place=self.expected_place)
+		scheduled_at = frappe.utils.data.get_datetime(self.scheduled_at_dt)
+		scheduled_at = scheduled_at.replace(tzinfo=pytz.timezone(frappe.utils.get_time_zone()))
+		scheduled_at = scheduled_at.utcnow().isoformat()+"Z"
 
-		self.lookup_id = new_hypertrack_action.lookup_id
-		self.metadata = new_hypertrack_action.metadata
+		place = frappe.get_doc("HyperTrack Place", self.expected_place)
+		expected_place = { "address": place.address }
 
-	def on_update(self):
-		hypertrack = get_hypertrack()
-		action = hypertrack.Action.retrieve(self.hypertrack_id)
+		if not self.hypertrack_id:
+			new_hypertrack_action = hypertrack.Action.create(
+					type=self.type,
+					scheduled_at=scheduled_at,
+					expected_place=expected_place,
+				)
 
-		action.type = self.type
-		action.lookup_id = self.lookup_id
-		action.scheduled_at = self.scheduled_at
-		action.expected_place = self.expected_place
-		action.metadata = self.metadata
+			self.lookup_id = new_hypertrack_action.lookup_id
+			self.metadata = new_hypertrack_action.metadata
+			self.name = new_hypertrack_action.id
+			self.hypertrack_id = new_hypertrack_action.id
+		else:
+			action = hypertrack.Action.retrieve(self.hypertrack_id)
 
-		action.save()
+			action.type = self.type
+			action.lookup_id = self.lookup_id
+			action.scheduled_at = self.scheduled_at
+			action.expected_place = self.expected_place
+			action.metadata = self.metadata
 
-	def on_delete(self):
+	def on_trash(self):
 		hypertrack = get_hypertrack()
 		action = hypertrack.Action.retrieve(self.hypertrack_id)
 		action.delete()
