4d0d0e1ffc99967c3969319840ccaaa9242c11a0
Revant Nandgaonkar <revant.one@gmail.com>
2017-08-15 12:01:54 +0530

    user code cleanup

--- hypertrack_integration/hypertrack_integration/doctype/hypertrack_group/hypertrack_group.py
+++ hypertrack_integration/hypertrack_integration/doctype/hypertrack_group/hypertrack_group.py
@@ -14,38 +14,39 @@ class HyperTrackGroup(NestedSet):
 	nsm_parent_field = "parent_hypertrack_group"
 	
 	def validate(self):
-		if self.hypertrack_group_name != "All HyperTrack Groups" and not self.hypertrack_id:
-			hypertrack = HTGroup(frappe.db.get_value("HyperTrack Settings",None, "hypertrack_secret_key"))
-			parent_group_id = None
-			if self.parent_hypertrack_group and self.parent_hypertrack_group != "All HyperTrack Groups":
-				parent_group_id = frappe.db.get_value("HyperTrack Group", self.parent_hypertrack_group, "hypertrack_id")
+		if self.hypertrack_group_name != "All HyperTrack Groups": 
+			if not self.hypertrack_id:
+				hypertrack = HTGroup(frappe.db.get_value("HyperTrack Settings",None, "hypertrack_secret_key"))
+				parent_group_id = None
+				if self.parent_hypertrack_group and self.parent_hypertrack_group != "All HyperTrack Groups":
+					parent_group_id = frappe.db.get_value("HyperTrack Group", self.parent_hypertrack_group, "hypertrack_id")
 
-			new_hypertrack_group = hypertrack.create(
-				name = self.hypertrack_group_name,
-				parent_group_id = parent_group_id
-			)
+				new_hypertrack_group = hypertrack.create(
+					name = self.hypertrack_group_name,
+					parent_group_id = parent_group_id
+				)
 
-			self.created_at = new_hypertrack_group.get("created_at")
-			self.hypertrack_id = new_hypertrack_group.get("id")
-			self.modified_at = new_hypertrack_group.get("modified_at")
-			self.hypertrack_group_name = new_hypertrack_group.get("name")
+				self.created_at = new_hypertrack_group.get("created_at")
+				self.hypertrack_id = new_hypertrack_group.get("id")
+				self.modified_at = new_hypertrack_group.get("modified_at")
+				self.hypertrack_group_name = new_hypertrack_group.get("name")
 
-			parent_hypertrack_group = self.parent_hypertrack_group \
-				if self.parent_hypertrack_group == "All HyperTrack Groups" else None
+				parent_hypertrack_group = self.parent_hypertrack_group \
+					if self.parent_hypertrack_group == "All HyperTrack Groups" else None
 
-			if new_hypertrack_group.get("parent_group_id"):
-				parent_hypertrack_group = frappe.db.get_value("HyperTrack Group", {"hypertrack_id": new_hypertrack_group.get("parent_group_id")}, "hypertrack_group_name")
+				if new_hypertrack_group.get("parent_group_id"):
+					parent_hypertrack_group = frappe.db.get_value("HyperTrack Group", {"hypertrack_id": new_hypertrack_group.get("parent_group_id")}, "hypertrack_group_name")
 
-			self.parent_hypertrack_group = parent_hypertrack_group
-			self.token = new_hypertrack_group.get("token")
+				self.parent_hypertrack_group = parent_hypertrack_group
+				self.token = new_hypertrack_group.get("token")
 
-		elif self.hypertrack_group_name != "All HyperTrack Groups" and self.hypertrack_id:
-			hypertrack = HTGroup(frappe.db.get_value("HyperTrack Settings", None, "hypertrack_secret_key"))
-			updated_group = hypertrack.update(self.hypertrack_id, self.hypertrack_group_name)
-			for x in xrange(1,10):
-				print(updated_group or "Nogroup")
-			self.hypertrack_group_name = updated_group.get("name")
-			self.modified_at = updated_group.get("modified_at")
+			elif self.hypertrack_id:
+				hypertrack = HTGroup(frappe.db.get_value("HyperTrack Settings", None, "hypertrack_secret_key"))
+				updated_group = hypertrack.update(self.hypertrack_id, self.hypertrack_group_name)
+				for x in xrange(1,10):
+					print(updated_group or "Nogroup")
+				self.hypertrack_group_name = updated_group.get("name")
+				self.modified_at = updated_group.get("modified_at")
 
 	def on_trash(self):
 		if self.hypertrack_group_name != "All HyperTrack Groups":
